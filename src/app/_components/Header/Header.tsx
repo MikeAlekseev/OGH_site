b9f0166a42b618873112f2071b19258a0b2878b2
johhnynouz <johhnynouz@yandex.ru>
2025-12-11 21:27:02 +0300

    Добавлено лого

--- src/app/_components/Header/Header.tsx
+++ src/app/_components/Header/Header.tsx
@@ -1,11 +1,16 @@
 import Link from 'next/link'
 
+
 import { Menu } from '../Menu'
+
+import styles from './Header.module.css'
 
 export function Header() {
     return (
-        <header>
-            <h1><Link href="/">ОГХ</Link></h1>
+        <header className={styles.module}>
+            <p className="logo"><Link href="/">
+                <img className="logo" src='logo.png' alt="logo" />
+                Объединенное городское хозяйство</Link></p>
             <Menu />
         </header>
     )
