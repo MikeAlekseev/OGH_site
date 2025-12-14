93979f489be4cb463ac9010ea077e79e896815ce
Konstantin Grigorchuk <kgrigorchuk@i-novus.ru>
2025-12-14 21:37:56 +0300

    ci fix

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,6 +6,11 @@ module.exports = {
             name: 'OGH_site',
             script: 'node_modules/next/dist/bin/next',
             args: 'start',
+            instances: 1,
+            autorestart: true,
+            watch: false,
+            time: true,
+
             env_production: {
                 NODE_ENV: 'production',
             },
